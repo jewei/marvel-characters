ae4d78babf9b247aa5a8f3af5919c539badd30fe
Jewei Mak <jewei.mak@gmail.com>
2016-03-11 16:42:22 +0800

    feat(random): Add ability to get an array of Marvel character names
    If you pass a number to the random function, you will receive an array with that number of random
    items
    
    closes #2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 var characterNames = require('./characters.json');
 var uniqueRandomArray = require('unique-random-array');
+var getRandomItem = uniqueRandomArray(characterNames);
 
 module.exports = {
   all: characterNames,
-  random: uniqueRandomArray(characterNames)
+  random: random
 };
+
+function random(number) {
+  if (number === undefined) {
+    return getRandomItem();
+  } else {
+    var randomItems = [];
+    for (var i = 0; i < number; i++) {
+      randomItems.push(getRandomItem());
+    }
+    return randomItems;
+  }
+}
--- src/index.test.js
+++ src/index.test.js
@@ -13,7 +13,7 @@ describe('marvel-comics-characters', function() {
       }
     });
 
-    it('should contain `Captain America`', function(){
+    it('should contain `Captain America`', function() {
         expect(marvel.all).to.include('Captain America');
     });
   });
@@ -22,6 +22,14 @@ describe('marvel-comics-characters', function() {
     it('should return a random character from marvel.all', function() {
       var random = marvel.random();
       expect(marvel.all).to.include(random);
+    });
+
+    it('should return an array of items if passed a number', function() {
+      var randoms = marvel.random(3);
+      expect(randoms).to.have.length(3);
+      randoms.forEach(function(item) {
+        expect(marvel.all).to.include(item);
+      });
     });
   });
 });
